e3dad5151098f76bee201a558343d73ea2735e54
Harley Johnson <62576957+JohnsonHarleyR@users.noreply.github.com>
2021-01-18 01:37:00 -0500

    Initial commit - formed the skeleton

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace BMICalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            Validator validator = new Validator();
+
+
+            // title text
+            Console.WriteLine("BMI Calculator");
+
+            // A loop that continues until user types 'no' when asked to continue
+            while (true) // keep going unless the loop is broken 
+            {
+                // variables
+                string unit;
+                double height;
+                double weight;
+                double bMI;
+
+                string input;
+                bool isValid = false;
+
+                // first ask if they want to enter metric or standard
+                // keep looping until they give a valid answer
+                while (!isValid)
+                { }
+
+                // set isValid back to false for the next time it's needed
+                isValid = false;
+
+                // if it's metric, go through this
+                if (input == "metric")
+                {
+                    // get their height in centimeters
+                    // use another loop to validate that they entered a valid number in acceptable range
+
+                    // get their weight in kg
+                    // use another loop to validate that they entered a valid number in acceptable range
+
+                }
+                // else if it's standard, go through this
+                else
+                {
+                    // get their height in feet
+                    // use another loop to validate that they entered a valid number in acceptable range
+
+                    // multiply the feet by 12 to add to their height
+
+                    // get the rest of the inches in their height
+                    // use another loop to validate that they entered a valid number in acceptable range
+
+                    // add the final inches to their height
+
+                    // get their weight in pounds
+                    // use another loop to validate that they entered a valid number in acceptable range
+
+                    // convert height to metric
+
+                    // convert weight to metric
+                }
+
+                // calculate the BMI
+
+            }
+
+
+        }
+
+        // methods
+
+        // convert inches to centimeters
+
+        // convert pounds to kg
+
+        // calculate their bmi
+
+        // calculate what health range their bmi is in
+
+        
+    }
+}
---
+++ Validator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BMICalculator
+{
+    class Validator
+    {
+        // methods to check validity
+
+        // did they enter a valid number for feet and inches for height?
+
+        // did they enter a valid number for kg?
+
+        // did they enter a valid Ibs number in the range?
+
+        // did they enter a valid kg number in the range?
+
+        // did they enter valid metric or standard text?
+        public bool IsValidMetricStandard(string input)
+        {
+            //if (input.Contains("m") || input.Contains("s")) // making it less picky for the user
+            if (input.Equals("metric", StringComparison.OrdinalIgnoreCase) ||
+                input.Equals("standard", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // did they enter a valid yes or no?
+        public bool IsValidYesNo(string input)
+        {
+            if (input.Equals("yes", StringComparison.OrdinalIgnoreCase) ||
+                input.Equals("no", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
